c32cdf676d156d2fb6cb05788714a10ae8f04341
Maykon Mendes <maykon.warlley@gmail.com>
2026-08-02 19:17:47 -0300

    feat atualizações no css

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -4,20 +4,22 @@ import styles from "./NavBar.module.css"
 import logo from "../../imagens/costs_logo.png"
 
 function NavBar() {
-    return (
-        <nav class={styles.navbar}>
-            <container>
-                <ul class={styles.list}>
-                    <li Class={styles.item}><Link to="/"><img src={logo} alt="logo"></img></Link></li>
-                    <li Class={styles.item}><Link to="/">Home</Link></li>
-                    <li Class={styles.item}><Link to="/Company">Empresa</Link></li>
-                    <li Class={styles.item}><Link to="/Contact">Contato</Link></li>
-                    <li Class={styles.item}><Link to="/Newproject">Novo Projeto</Link></li>
-                    <li Class={styles.item}><Link to="/projects">Meus Projetos</Link></li>
-                </ul>
-            </container>
-        </nav>
-
-    )
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/"><img src={logo} alt="logo" /></Link>
+          </li>
+          <li className={styles.item}><Link to="/">Home</Link></li>
+          <li className={styles.item}><Link to="/Company">Empresa</Link></li>
+          <li className={styles.item}><Link to="/Contact">Contato</Link></li>
+          <li className={styles.item}><Link to="/Newproject">Novo Projeto</Link></li>
+          <li className={styles.item}><Link to="/projects">Meus Projetos</Link></li>
+        </ul>
+      </Container>
+    </nav>
+  )
 }
+
 export default NavBar
