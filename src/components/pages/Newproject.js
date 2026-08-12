abc9fca6d9ea716aed3aefb488c6371f9af197d3
Maykon Mendes <maykon.warlley@gmail.com>
2026-08-12 11:43:32 -0300

    feat: Adiciona mensagem de sucesso e erro, atualiza estilos e corrige criação de projetos

--- src/components/pages/Newproject.js
+++ src/components/pages/Newproject.js
@@ -3,34 +3,34 @@ import { useNavigate } from 'react-router-dom';
 import styles from './NewProject.module.css'
 import ProjectForm from '../Project/ProjectForm'
 
-function NewProject(){
+function NewProject() {
 
-    const navigate= useNavigate()
+    const navigate = useNavigate()
 
-    function createPost(project){
+    function createPost(project) {
         // initialize cost and services
-        project.cost=0
-        project.services=[]
+        project.cost = 0
+        project.services = []
 
         fetch("http://localhost:5000/projects", {
             method: 'POST',
-            headers:{
+            headers: {
                 'content-type': 'application/json',
             },
             body: JSON.stringify(project)
         })
-        .then((resp) => resp.json())
-        .then((data) =>{
-            console.log(data)
-            //redirect
-            navigate.push('/project', {message: 'projeto criado com sucesso'})
-        })
-        .catch(err => console.log(err))
+            .then((resp) => resp.json())
+            .then((data) => {
+    console.log(data);
+    navigate('/projects', { state: { message: 'Projeto criado com sucesso!' } });
+})
+
+            .catch(err => console.log(err))
 
     }
 
-    return(
-    <div className={styles.newproject_container}>
+    return (
+        <div className={styles.newproject_container}>
             <h1>Criar projeto</h1>
             <p>Crie seu Projeto para depois adicionar os serviços</p>
             <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,18 @@
+import { useLocation } from "react-router-dom"
+import Message from "../Layout/Message"
+
 function Projects(){
+    const location = useLocation()
+    let message= ''
+    if (location.state) {
+        message = location.state.message
+    }
+
     return(
         <div>
-            <h1>My projects</h1>
+            <h1>Meus projetos</h1>
+            {message && <Message type="sucess" msg={message} />}
+
         </div>
     )
 }
